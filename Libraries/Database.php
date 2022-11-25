49a2ceaae49bf38510f867804453d5bf77c4213b
nickr <nicholaspriegel@gmail.com>
2022-11-25 13:53:09 -0500

    .

--- Libraries/Database.php
+++ Libraries/Database.php
@@ -6,19 +6,17 @@ class Database{
   
   public function __construct()
   {
-    // $host   = 'containers-us-west-69.railway.app';
-    $host   = 'localhost';
-    // $dbname = 'railway';
-    $dbname = 'sessionsOOP';
-    $user   = 'root';
-    // $pword  = 'xbGPoYyhG4EtgOzPSVDL';
-    $pword  = '';
-    // $port   = 7912;
-    $port   = 3306;
-    $dsn    = 'mysql:host='.$host.';port='.$port.';dbname='.$dbname ;
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
+
+    $dsn    = 'mysql:host='.$host.';dbname='.$db ;
 
     try {
-      $this->pdo = new PDO($dsn, $user, $pword);
+      $this->pdo = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
       die('The error is: ' . $e->getMessage());
     }
