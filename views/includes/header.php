d96d0f756bcb15880d1ad8e18c1f1b05aa4d20cd
nickr <nicholaspriegel@gmail.com>
2023-04-03 20:19:30 -0400

    .

--- views/includes/header.php
+++ views/includes/header.php
@@ -8,14 +8,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <style>
-    /* * {
-      border: black 1px solid;
-    } */
-    body {
-      background-color: gray;
-    }
-  </style>
+  <style>body{background-color: gray;}</style>
   <title>PHP Blog</title>
 </head>
 
