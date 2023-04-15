759f4e14715216888b56029e1d3b38ad24df9021
nickr <nicholaspriegel@gmail.com>
2023-04-15 12:51:16 -0400

    .

--- views/home.php
+++ views/home.php
@@ -6,21 +6,21 @@
 
 ?>
 
-<!-- <h1 style='margin: auto; text-align: center;'>Home page</h1>
-<p style='margin: auto; text-align: center;'>Project only has minimal styling at the moment, but it is fully functional.</p>
-<p style='margin: auto; text-align: center;'>See <a href="https://github.com/Nicholas-Riegel/php_blog">README file on the GitHub repository</a>.</p>
-<p style='margin: auto; text-align: center;'>Feel free to register, login and create a post!</p> -->
 
 <?php foreach ($data as $post) : ?>
-  
-  <h4><?= $post['username'] ?></h4>
-  <p><?= $post['body'] ?></p>
+  <div class="container card p-3 mb-3">
 
-  <?php if ($post['username'] == $session_username) : ?>
-    <a href='/pages/edit/?post_id=<?= $post['post_id'] ?>'>Edit</a>
-    <a onClick="return confirm('Are you sure you want to delete this?')" href='/posts/delete/?post_id=<?= $post['post_id'] ?>' >Delete</a>
-  <?php endif ?>
+    <p class="mb-0">Author:</p>
+    <h4 class="border rounded ps-1 pe-1"><?= $post['username'] ?></h4>
+    <p class="mb-0">Post:</p>
+    <p class="border rounded ps-1 pe-1"><?= $post['body'] ?></p>
     
+    <?php if ($post['username'] == $session_username) : ?>
+      <a href='/pages/edit/?post_id=<?= $post['post_id'] ?>'>Edit</a>
+      <a onClick="return confirm('Are you sure you want to delete this?')" href='/posts/delete/?post_id=<?= $post['post_id'] ?>' >Delete</a>
+    <?php endif ?>
+      
+  </div>
 <?php endforeach ?>
 
 <?php require './views/includes/footer.php'; ?>
--- views/includes/header.php
+++ views/includes/header.php
@@ -1,66 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
   
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  
+  <head>
+    
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
   <!-- Bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
   
-  <link rel="stylesheet" href="./views/css/styles.css">
+  <link rel="stylesheet" href=<?= URL_ROOT ."/views/css/styles.css"?>>
   
   <title>PHP Blog</title>
-
+  
 </head>
 
 <body>
-
+  
   <nav class="navbar fixed-top bg-secondary">
+    <div class="container">
 
-    <?php if (!empty($_SESSION['username'])) : ?>
-      
-      <!-- <a href="/pages/logout" style="float: right; margin-left: 10px;">Log out</a> -->
-      <a href="/pages/logout" class="text-decoration-none">
-        <h1 class="display-6 text-info">Log out</h1>
+      <a href="/pages/index" class="text-decoration-none">
+        <h1 class="display-6 text-info">Home</h1>
       </a>
       
+      <?php if (!empty($_SESSION['username'])) : ?>
+      
+        <a href="/pages/create">
+          <h1 class="display-6 text-info">Create Post</h1>
+        </a>
+      
+      <?php endif ?>
+      
+      <h4 class="display-6 text-info"><?= !empty($_SESSION['username']) ? $_SESSION['username'] : 'Guest' ?></h4>
+      
+      <?php if (!empty($_SESSION['username'])) : ?>
+        
+        <a href="/pages/logout" class="text-decoration-none">
+          <h1 class="display-6 text-info">Log out</h1>
+        </a>
+        
       <?php else : ?>
         
-        <!-- <a href="/pages/register" style='margin-left: 10px;float: right'>Register</a> -->
         <a href="/pages/register" class="text-decoration-none">
           <h1 class="display-6 text-info">Register</h1>
         </a>
-
-        <!-- <a href="/pages/login" style='margin-left: 10px; float: right'>Log In</a> -->
+        
         <a href="/pages/login" class="text-decoration-none">
-          <h1 class="display-6 text-info">
-            Log In
-          </h1>  
+          <h1 class="display-6 text-info">Log In</h1>  
         </a>
         
-        <?php endif ?>
-        
-        <!-- <h4 style='float: right; margin: 0;'><?= !empty($_SESSION['username']) ? $_SESSION['username'] : 'Guest' ?></h4> -->
-        <h4 class="display-6 text-info"><?= !empty($_SESSION['username']) ? $_SESSION['username'] : 'Guest' ?></h4>
-        
-        <a href="/pages/index" class="text-decoration-none">
-          <h1 class="display-6 text-info">
-            Home
-          </h1>
-        </a>
-        
-        <?php if (!empty($_SESSION['username'])) : ?>
-          
-          <a href="/pages/create">
-            <h1 class="display-6 text-info">
-              Create Post
-            </h1>
-          </a>
-          
-        <?php endif ?>
-        
-        <hr>
-    </nav>  
+      <?php endif ?>
+
+    </div>
+  </nav>  
+  <main style="position: relative; top: 6rem;">
