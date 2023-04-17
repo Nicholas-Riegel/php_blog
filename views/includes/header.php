7b469d526b555613b83a20a2998738a60ba7436f
nickr <nicholaspriegel@gmail.com>
2023-04-16 22:54:42 -0400

    .

--- views/includes/header.php
+++ views/includes/header.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
   
-  <head>
+<head>
     
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
   <!-- Bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-  
-  <link rel="stylesheet" href=<?= URL_ROOT ."/views/css/styles.css"?>>
+
+  <!-- CSS -->
+  <link rel="stylesheet" href=<?= URL_ROOT . "/views/css/styles.css" ?>>
   
   <title>PHP Blog</title>
   
@@ -19,12 +20,15 @@
 <body>
   
   <nav class="navbar fixed-top bg-secondary">
-    <div class="container">
+    
+    <div class="container d-flex">
 
-      <a href="/pages/index" class="text-decoration-none">
+      <!-- Home link  -->
+      <a href="/pages/index" class="text-decoration-none me-auto">
         <h1 class="display-6 text-info">Home</h1>
       </a>
       
+      <!-- Create Post link  -->
       <?php if (!empty($_SESSION['username'])) : ?>
       
         <a href="/pages/create">
@@ -33,26 +37,33 @@
       
       <?php endif ?>
       
+      <!-- User Name  -->
       <h4 class="display-6 text-info"><?= !empty($_SESSION['username']) ? $_SESSION['username'] : 'Guest' ?></h4>
       
       <?php if (!empty($_SESSION['username'])) : ?>
         
+        <!-- Logout link  -->
         <a href="/pages/logout" class="text-decoration-none">
           <h1 class="display-6 text-info">Log out</h1>
         </a>
         
       <?php else : ?>
         
-        <a href="/pages/register" class="text-decoration-none">
-          <h1 class="display-6 text-info">Register</h1>
-        </a>
-        
-        <a href="/pages/login" class="text-decoration-none">
+        <!-- Log in link  -->
+
+        <a href="/pages/login" class="text-decoration-none ms-4">
           <h1 class="display-6 text-info">Log In</h1>  
         </a>
         
-      <?php endif ?>
+        <!-- Register link  -->
+        <a href="/pages/register" class="text-decoration-none ms-4">
+          <h1 class="display-6 text-info">Register</h1>
+        </a>
 
+      <?php endif ?>
+      
     </div>
+  
   </nav>  
+
   <main style="position: relative; top: 6rem;">
--- views/register.php
+++ views/register.php
@@ -1,19 +1,21 @@
 <?php require './views/includes/header.php'; ?>
 
-<h1 style='margin: auto; text-align: center;'>Register Page</h1>
+<h1 style='margin: auto; text-align: center;'>Register</h1>
 
-<form action="/users/register" method="POST">
-
-  <label for="username">Username:</label>
-  <br>
-  <input type="text" name="username" id='username'>
-  <br>
-  <label for="password">Password:</label>
-  <br>
-  <input type="text" name="password" id='password'>
-  <br>
-  <button name='submit_registration' value=1>Submit</button>
-
-</form>
+<div class="container card p-3 mb-3">
+  <form action="/users/register" method="POST">
+    <label for="username">Username:</label>
+    <br>
+    <input type="text" name="username" id='username' class="border rounded ps-1 pe-1 w-100">
+    <br>
+    <label for="password">Password:</label>
+    <br>
+    <input type="text" name="password" id='password' class="border rounded ps-1 pe-1 w-100">
+    <br>
+    <div class="d-flex">
+      <button name='submit_registration' value=1 class='btn btn-md btn-outline-info ms-auto'>Submit</button>
+    </div>
+  </form>
+</div>
 
 <?php require './views/includes/footer.php'; ?>
