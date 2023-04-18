e164b0c748388cbf2e5d893e75743e436d598217
nickr <nicholaspriegel@gmail.com>
2023-04-18 00:09:09 -0400

    .

--- views/login.php
+++ views/login.php
@@ -2,18 +2,24 @@
 
 <h1 style='margin: auto; text-align: center;'>Login Page</h1>
 
-<form action="/users/login" method="POST">
+<div class="container card p-3 mb-3">
 
-  <label for="username">Username:</label>
-  <br>
-  <input type="text" name="username" id='username'>
-  <br>
-  <label for="password">Password:</label>
-  <br>
-  <input type="text" name="password" id='password'>
-  <br>
-  <button name='submit_login' value=1>Submit</button>
+  <form action="/users/login" method="POST">
 
-</form>
+    <label for="username">Username:</label>
+    <br>
+    <input type="text" name="username" id='username' class="border rounded ps-1 pe-1 w-100">
+    <br>
+    <label for="password">Password:</label>
+    <br>
+    <input type="text" name="password" id='password' class="border rounded ps-1 pe-1 w-100">
+    <br>
+    <div class="d-flex">
+      <button name='submit_login' value=1 class='btn btn-md btn-outline-info ms-auto'>Submit</button>
+    </div>
+    
+  </form>
+
+</div>
 
 <?php require './views/includes/footer.php'; ?>
